a2bf6d37c0a2dfefc5b624181c876abf295d1f70
Yashaswi Anand <yashaswi@upjai.ai>
2025-03-30 23:23:39 +0530

    url change

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
 const config = {
-    endpoint: "http://localhost:7000"
+    // endpoint: "http://localhost:7000"
+    endpoint: 'https://blog-project-backend-9ma0.onrender.com'
 }
 
 window.config = config
