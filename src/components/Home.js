0c60c3fb6ebf3ff6cf98971f0dcb032e5559ee35
Yashaswi Anand <yashaswi@upjao.ai>
2025-04-15 23:23:12 +0530

    social add on side

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,18 @@ import { Box } from '@mui/material'
 import MostViewsBlog from '../Widgets/MostViewsBlog'
 import LatestBlog from '../Widgets/LatestBlog'
 import HomeBlog from '../Widgets/HomeBlog'
+import Socials from '../Widgets/fancyWidgets/Socials'
 
 function Home() {
   return (
     <Box>
       <main >
-        <HomeBlog/>
-        <LatestBlog/>
-        <MostViewsBlog/>
+        {/* Social Media */}
+        <Socials />
+        {/* Pages */}
+        <HomeBlog />
+        <LatestBlog />
+        <MostViewsBlog />
       </main>
     </Box>
   )
