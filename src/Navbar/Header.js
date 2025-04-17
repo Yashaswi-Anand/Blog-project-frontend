9fd929c5fe911667fcc03ee68f5d8f392fc47f49
Yashaswi Anand <yashaswi@upjao.ai>
2025-04-17 14:16:31 +0530

    socila added on description page

--- src/Navbar/Header.js
+++ src/Navbar/Header.js
@@ -5,8 +5,8 @@ import MenuIcon from "@mui/icons-material/Menu";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useNavigate } from "react-router-dom";
 import LoginPage from "../Widgets/LoginPage";
-import { Facebook, LinkedIn, Instagram, YouTube } from '@mui/icons-material';
 import ReplyIcon from '@mui/icons-material/Reply';
+import SocialOnFooter from "../Widgets/fancyWidgets/SocialOnFooter";
 
 function Header() {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -52,13 +52,13 @@ function Header() {
       <Box>
         <LoginPage open={open} setOpen={setOpen} />
       </Box>
-      <Box sx={{ 
-        width: isTabletOrMobile ? "109%" : "80%", 
-        display: "flex", 
-        justifyContent: "space-between", 
-        alignItems: "center", 
-        marginRight: isTabletOrMobile ? "30px" : "0%", 
-        }}>
+      <Box sx={{
+        width: isTabletOrMobile ? "109%" : "80%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginRight: isTabletOrMobile ? "30px" : "0%",
+      }}>
         <Typography variant="h6" sx={{ fontWeight: "bold", cursor: "pointer" }} onClick={() => handleNavigation('/')}>
           <span style={{ color: "#00bcd4" }}>thetech</span>Khazana
         </Typography>
@@ -97,16 +97,16 @@ function Header() {
                 <Box sx={{ padding: '0px 10px' }}>
                   <List>
                     <ListItem button onClick={() => handleNavigation('/gadget')}>
-                      <ListItemText sx={{':hover' : { color: '#00bcd4', textDecoration: 'underline' }}} primary="Gadget" />
+                      <ListItemText sx={{ ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} primary="Gadget" />
                     </ListItem>
                     <ListItem button onClick={() => handleNavigation('/ai_website')}>
-                      <ListItemText sx={{':hover' : { color: '#00bcd4', textDecoration: 'underline' }}} primary="AI Website" />
+                      <ListItemText sx={{ ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} primary="AI Website" />
                     </ListItem>
                     <ListItem button onClick={() => handleNavigation('/apps')}>
-                      <ListItemText sx={{':hover' : { color: '#00bcd4', textDecoration: 'underline' }}} primary="Apps" />
+                      <ListItemText sx={{ ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} primary="Apps" />
                     </ListItem>
                     <ListItem button onClick={() => handleNavigation('/tips')}>
-                      <ListItemText sx={{':hover' : { color: '#00bcd4', textDecoration: 'underline' }}} primary="Tips & Trick" />
+                      <ListItemText sx={{ ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} primary="Tips & Trick" />
                     </ListItem>
                     {localStorage.getItem('isLoggedIn') === 'true' && <ListItem button onClick={() => handleNavigation('/add-new-post')}>
                       <ListItemText primary="Add Post" />
@@ -123,27 +123,18 @@ function Header() {
 
               <Box>
                 <hr></hr>
-                <IconButton sx={{ color: '#1877F2', mx: 0.5 }}>
-                  <Facebook sx={{ fontSize: '25px' }} />
-                </IconButton>
-                <IconButton sx={{ color: '#000080', mx: 0.5 }}>
-                  <LinkedIn sx={{ fontSize: '25px' }} />
-                </IconButton>
-                <IconButton sx={{ color: 'gray', mx: 0.5 }}>
-                  <Instagram sx={{ fontSize: '25px' }} />
-                </IconButton>
-                <IconButton sx={{ color: 'red', mx: 0.5 }}>
-                  <YouTube sx={{ fontSize: '35px' }} />
-                </IconButton>
+                <Box sx={{ display: 'flex', justifyContent: 'end', alignItems: 'center', padding: '5px 10px' }}>
+                  <SocialOnFooter />
+                </Box>
               </Box>
             </Drawer>
           </>
         ) : (
           <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
-            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover' : { color: '#00bcd4', textDecoration: 'underline' } }} onClick={() => handleNavigation('/gadget')}>Gadget</Typography>
-            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover' : { color: '#00bcd4', textDecoration: 'underline' }}} onClick={() => handleNavigation('/ai_website')}>AI Website</Typography>
-            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover' : { color: '#00bcd4', textDecoration: 'underline' } }} onClick={() => handleNavigation('/apps')}>Apps</Typography>
-            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover' : { color: '#00bcd4', textDecoration: 'underline' } }} onClick={() => handleNavigation('/tips')}>Tips & Trick</Typography>
+            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} onClick={() => handleNavigation('/gadget')}>Gadget</Typography>
+            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} onClick={() => handleNavigation('/ai_website')}>AI Website</Typography>
+            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} onClick={() => handleNavigation('/apps')}>Apps</Typography>
+            <Typography variant="body1" sx={{ cursor: "pointer", fontWeight: 500, ':hover': { color: '#00bcd4', textDecoration: 'underline' } }} onClick={() => handleNavigation('/tips')}>Tips & Trick</Typography>
             {localStorage.getItem('isLoggedIn') && <Button variant="outlined" sx={{ cursor: "pointer", fontWeight: 500 }} onClick={() => handleNavigation('/add-new-post')}><strong> + New Post </strong></Button>}
             {localStorage.getItem('isLoggedIn') === 'true'
               ? <Button variant="outlined" color="error" onClick={() => handleLogout()}> <strong>Logout</strong> </Button>
