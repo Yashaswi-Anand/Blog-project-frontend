8c9087cf5799a54689eb5b19f2ae2473be602804
Yashaswi Anand <yashaswi@upjao.ai>
2025-04-20 22:40:31 +0530

    modify url

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 const config = {
     // endpoint: "http://localhost:7000"
-    // endpoint: 'https://blog-project-backend-9ma0.onrender.com'
-    endpoint: 'https://blog-project-backend-production.up.railway.app'
+    endpoint: 'https://blog-project-backend-9ma0.onrender.com'
+    // endpoint: 'https://blog-project-backend-production.up.railway.app'
 }
 
 window.config = config
