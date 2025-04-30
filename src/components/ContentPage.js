f9430edbdfc625db5f9e88c753b703de277abffd
Yashaswi Anand <yashaswi@upjao.ai>
2025-04-30 23:29:19 +0530

    modify

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Grid, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Container, Grid, useMediaQuery, useTheme } from '@mui/material';
 import ContentBlog from '../Widgets/ContentBlog';
 import ContentSideBlog from '../Widgets/ContentSideBlog';
 
@@ -8,21 +8,25 @@ function ContentPage() {
   const isMobile = useMediaQuery(theme.breakpoints.down('md')); // Hide on md and below
 
   return (
-    <Container sx={{ marginTop: 10 }}>
-      <Grid container spacing={4} backgroundColor={theme.palette.background.paper}>
-        {/* Left Sidebar - Latest Stories (Hidden on Mobile) */}
-        {!isMobile && (
-          <Grid item xs={12} md={4}>
-            <ContentSideBlog />
-          </Grid>
-        )}
+    <Box sx={{ margin: isMobile ? '0px' : '0px 10%' }}>
+      <Box backgroundColor={theme.palette.background.paper}>
+        <Container sx={{ marginTop: 10 }}>
+          <Grid container spacing={4}>
+            {/* Left Sidebar - Latest Stories (Hidden on Mobile) */}
+            {!isMobile && (
+              <Grid item xs={12} md={4}>
+                <ContentSideBlog />
+              </Grid>
+            )}
 
-        {/* Main Content - Trending News */}
-        <Grid item xs={12} md={isMobile ? 12 : 8} backgroundColor={theme.palette.background.paper}>
-          <ContentBlog content_type={window.location.pathname.split('/').pop()} />
-        </Grid>
-      </Grid>
-    </Container>
+            {/* Main Content - Trending News */}
+            <Grid item xs={12} md={isMobile ? 12 : 8}>
+              <ContentBlog content_type={window.location.pathname.split('/').pop()} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
   );
 }
 
