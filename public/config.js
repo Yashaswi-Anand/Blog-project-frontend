8a13f34aa0e003b6d879d202dd728bd6e06134d2
Yashaswi Anand <yashaswi@upjao.ai>
2025-05-22 23:59:55 +0530

    modify backend url

--- public/config.js
+++ public/config.js
@@ -1,7 +1,8 @@
 const config = {
     // endpoint: "http://localhost:8080"
     // endpoint: 'https://blog-project-backend-9ma0.onrender.com'
-    endpoint: 'https://blog-project-backend-production.up.railway.app'
+    // endpoint: 'https://blog-project-backend-production.up.railway.app'
+    endpoint: 'http://13.126.16.218:8080'
 }
 
 window.config = config
